b19f995420cb2a959e0b7e0ad676b4c1342617f3
Nileshnhe <nehenilesh111@gmail.com>
2026-03-21 15:22:46 +0530

    Refactor app.js and index.html for improved button functionality and styling; update CSS for layout adjustments

--- Event/app.js
+++ Event/app.js
@@ -1,7 +1,23 @@
-let btn = document.querySelector('#btn1')
+let h1 = document.querySelector('h1');
+let cm = "Light";
+let resetbtn = document.querySelector('button')
 
-btn.onclick = () => {
-    console.log("btn was clicked")
-    
-    btn.innerHTML = (btn.innerHTML === "Hey") ? "click me" : "Hey";
-}
+
+h1.addEventListener("click", () => {
+    if (cm === "Light") {
+        cm = "Dark";
+        document.body.style.backgroundColor = "red";
+    } else {
+        cm = "Light";
+        document.body.style.backgroundColor = "black";
+    }
+    h1.innerHTML = cm;
+    console.log(cm);
+resetbtn.addEventListener("click", ()=>{
+    document.body.style.backgroundColor = "#212121";
+
+    cm = "Light"
+    h1.innerHTML = "Light"
+})
+
+});
